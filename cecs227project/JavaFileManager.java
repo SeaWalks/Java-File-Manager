ed677208abf643065d72e9dcaa0a959405a4e6cc
SeaWalk <ericchhour@outlook.com>
2021-04-08 15:19:42 -0700

    Test push

--- cecs227project/JavaFileManager.java
+++ cecs227project/JavaFileManager.java
@@ -5,6 +5,7 @@ public class JavaFileManager {
     public static void main(String[] args) {
         App app = new App();
         app.go();
+        System.out.println("BigBootyBallin");
     }
 
 }
