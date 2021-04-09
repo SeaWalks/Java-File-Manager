6c55a3c9f4e10c3f1e417ab7a57faf1a32fb843a
SeaWalk <ericchhour@outlook.com>
2021-04-08 18:42:31 -0700

    Test changes

--- cecs227project/JavaFileManager.java
+++ cecs227project/JavaFileManager.java
@@ -4,6 +4,7 @@ public class JavaFileManager {
     public static void main(String[] args) {
         App app = new App();
         app.go();
-        System.out.println("CocaCola");
+        System.out.println("Albertsons");
+        System.out.println("Test Change");
     }
 }
