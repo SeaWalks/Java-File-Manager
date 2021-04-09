ec5e50e006c34d414f704a829f25b2632913d0e8
SeaWalk <ericchhour@outlook.com>
2021-04-08 19:24:35 -0700

    More github testing

--- cecs227project/JavaFileManager.java
+++ cecs227project/JavaFileManager.java
@@ -4,7 +4,6 @@ public class JavaFileManager {
     public static void main(String[] args) {
         App app = new App();
         app.go();
-        System.out.println("Albertsons");
-        System.out.println("Test Change");
+       
     }
 }
