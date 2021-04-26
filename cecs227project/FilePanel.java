8825a08feed5195879f1a4fc8ccef761d4b67dc0
SeaWalk <ericchhour@outlook.com>
2021-04-26 13:13:41 -0700

    Death

--- cecs227project/FilePanel.java
+++ cecs227project/FilePanel.java
@@ -1,18 +1,18 @@
-package cecs227project;
-
 import javax.swing.*;
 import java.io.File;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 
+
 public class FilePanel extends JPanel {
     private static final long serialVersionUID = 4085105696717794672L;
     private JScrollPane scrollPane = new JScrollPane();
     JList<String> myList = new JList<String>(printDetails());
-
+    //public Dimension minimumSize = new Dimension(100, 50);
     public FilePanel() {
         scrollPane.setViewportView(myList);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        //scrollPane.setMinimumSize(minimumSize);
         add(scrollPane);
     }
 
