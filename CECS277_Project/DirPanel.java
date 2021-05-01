a1749f1b806725043f1401954260e0235bb20408
iyadhosn <80491883+iyadhosn@users.noreply.github.com>
2021-05-01 13:23:12 -0700

    Update DirPanel.java

--- CECS277_Project/DirPanel.java
+++ CECS277_Project/DirPanel.java
@@ -1,75 +1,112 @@
 package CECS277_Project;
 
-
 import javax.swing.*;
-import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import java.io.File;
-
+import javax.swing.event.*;
+import javax.swing.tree.*;
+import java.awt.*;
+import java.io.*;
 
 public class DirPanel extends JPanel {
-    JTree tree = new JTree();
-    JScrollPane scrollPane;
-    DefaultTreeModel treeModel;
+    private JScrollPane scrollPane;
+    private JTree tree;
+    private DefaultTreeModel treeModel;
+    private String currentDrive;
+    private File currentDirectory;
+    FileFrame BigFrame;
+    FilePanel RightPanel;
 
-    public DirPanel() {
-        buildTree();
-        tree.addTreeSelectionListener(new DemoTreeSelectionListener());
-        scrollPane = new JScrollPane();
-        scrollPane.setViewportView(tree);
-        add(scrollPane);
+    public void setFilePanel(FilePanel fp){
+        RightPanel = fp;
+    }
+    public void setFileFrame(FileFrame frame){
+        BigFrame = frame;
+    }
+    // Getters for active folders. Returns File objects.
+    public String getCurrentDrive() {
+        return currentDrive;
     }
 
-    private void buildTree() {
+    public File getCurrentDirectory() {
+        return currentDirectory;
+    }
 
-        tree.setShowsRootHandles(true);
-        MyFileNode base = new MyFileNode("C:\\Users\\Iyad\\Documents");
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode(base);
+    public JTree getTree() {
+        return tree;
+    }
 
-        File[] files = base.getFile().listFiles();
-        assert files != null;
-        for(File file: files){
-            MyFileNode currentFile = new MyFileNode(file.getName());
-            DefaultMutableTreeNode currentBranch = new DefaultMutableTreeNode(currentFile);
-            if(file.isDirectory()){
-                root.add(currentBranch);
-                createChild(file, currentBranch);
-            }else{
-                root.add(currentBranch);
-            }
-        }
+    public DirPanel(File base) {
+        FileNode root = new FileNode(base.getPath());
+        buildTree(root);
+        scrollPane = new JScrollPane();
+        scrollPane.setViewportView(tree);
+        this.setLayout(new BorderLayout());
+        this.add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void buildTree(FileNode base) {
+
+        tree = new JTree();
+        tree.addTreeSelectionListener(new DemoTreeSelectionListener());
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+        currentDirectory = base.getFile();
+        root.setUserObject(base);
+        createChildren(root);
         treeModel = new DefaultTreeModel(root);
         tree.setModel(treeModel);
     }
 
-    private void createChild(File fileRoot, DefaultMutableTreeNode node){
+    void createChildren(DefaultMutableTreeNode node) {
 
-        File[] files = fileRoot.listFiles();
-        //If there are no files (not a directory)
-        if (files == null) return;
-        //If there are subfiles (is a directory)
-        for (File file : files) {
-            DefaultMutableTreeNode childNode =
-                    new DefaultMutableTreeNode(new MyFileNode(file.getName()));
-            node.add(childNode);
-            if (file.isDirectory()) {
-                createChild(file, childNode); //Recursion
+        FileNode parentNode = (FileNode) node.getUserObject();
+        File parentFile = new File(parentNode.getFile().getPath());
+        File[] parentList = parentFile.listFiles();
+
+
+        if (parentList != null) {
+            for (File file : parentList) {
+                if (file.isDirectory() && (file.listFiles()!= null)) {
+                    DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(new FileNode(file.getPath()));
+                    node.add(childNode);
+                    File childFile = new File(file.getPath());
+                    File[] childList = childFile.listFiles();
+                    if (childList != null) {
+                        for (File file2 : childList) {
+                            if (file2.isDirectory()) {
+                                DefaultMutableTreeNode secondChildNode = new DefaultMutableTreeNode(
+                                        new FileNode(file2.getPath()));
+                                childNode.add(secondChildNode);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
-
     public class DemoTreeSelectionListener implements TreeSelectionListener {
+
         @Override
         public void valueChanged(TreeSelectionEvent e) {
-            System.out.println("You selected a node in the tree");
-            System.out.println(tree.getMinSelectionRow());
-            System.out.println(tree.getSelectionPath());
-            DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-           // MyFileNode mfn = (MyFileNode) node.getUserObject();
-            System.out.println(node);
+
+            DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+            FileNode nodeInfo = (FileNode) selectedNode.getUserObject();
+            File currentFile = new File(nodeInfo.getFile().getPath());
+            //Update RightPanel
+            RightPanel.FillList(currentFile);
+            RightPanel.setShowDetails(true);
+            BigFrame.setTitle(currentFile.getPath());
+			/*Debugging tools
+			System.out.println("The currently selected file name is: " + currentFile.getAbsolutePath());
+			System.out.println("The selected file is directory: " + currentFile.isDirectory());
+			System.out.println("Number of Children: " + selectedNode.getChildCount());
+			*/
+            if (currentFile.isDirectory()) {
+                currentDirectory = currentFile;
+                selectedNode.removeAllChildren();
+                createChildren(selectedNode);
+                //Debugging tools:
+                //System.out.println("DirPanel's current directory: "+ currentDirectory.getName());
+            }
         }
     }
 }
