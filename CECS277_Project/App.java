a7600f12bdda385a0135c1e145f3551951d44ede
iyadhosn <80491883+iyadhosn@users.noreply.github.com>
2021-05-01 13:22:17 -0700

    Update App.java

--- CECS277_Project/App.java
+++ CECS277_Project/App.java
@@ -1,51 +1,46 @@
 package CECS277_Project;
-import javax.swing.*;
+
+import java.awt.event.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.*;
+import java.io.File;
+import javax.swing.*;
 
-class App extends JFrame{
-    JPanel panel, topPanel;
-    JButton details, simple;
-    JMenuBar menuBar, statusBar;
+/******************
+ ToDo:
+ 1) FilePanel
+ Maybe add icons if I feel like it
+ 2) Toolbar
+ ComboBox -> Just need to link (use FilePanel.setShowDetails)
+ Simple   -> Just need to link (use FilePanel.setShowDetails)
+ Details  -> Just need to link (use FilePanel.setShowDetails)
+ 3) RightClick Menu
+ Rename   -> Just need to test and link. (Use FilePanel.renameFile)
+ Copy     -> Just need to link. (Use FilePanel.setCopiedFile)
+ Paste    -> Just need to link. (Use FilePanel.pasteFile)
+ Delete   -> Just need to test and link. (Use FilePanel.deleteFile)
+ 4) Menubar Items
+ Rename   -> Just need to test and link. (Use FilePanel.renameFile)
+ Copy     -> Just need to link. (Use FilePanel.setCopiedFile)
+ Delete   -> Just need to test and link. (Use FilePanel.deleteFile)
+ Run      -> Just need to link (use FilePanel.runFile)
+ 5) Fix "New" button to spawn at a specific location- (0,100)
+ FileFrame edited to take in parameters. When you create a new FileFrame
+ in the buttonjust add in the parameters, ex new ff(0,100) or whatever
+ Then we are done, just organize/clean up code and commenting!!!!
+ 3 warnings to fix from professors code. Arraylists need to be parameterized
+ *******************/
+
+class App extends JFrame {
+    JPanel mainPanel, topPanel;
+    JPanel toolbarPanel;
     JDesktopPane desktopPane;
+    JMenuBar mb, statusBar;
+    JButton details, simple;
+    JComboBox toolbarBox;
 
-    public App(){
-        panel = new JPanel();
-        topPanel = new JPanel();
-        desktopPane = new JDesktopPane();
-        menuBar = new JMenuBar();
-        statusBar = new JMenuBar();
+    FileFrame ff;
 
-        details = new JButton("Details");
-        simple = new JButton("Simple");
-
-        panel.setLayout(new BorderLayout());
-    }
-
-    public void go() {
-        buildMenu();
-        buildStatusBar("C:");//change somehow
-        buildToolbar();
-
-        this.add(panel);
-
-        FileFrame ff = new FileFrame();
-
-        this.setTitle("277 File Manager Project");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        setVisible(true);
-        this.setSize(800, 600);
-        
-        panel.add(topPanel, BorderLayout.NORTH);
-        panel.add(desktopPane, BorderLayout.CENTER);
-
-        desktopPane.add(ff);
-    }
-
-    public void buildMenu(){
+    private void buildMenuBar() {
         JMenu fileMenu, treeMenu, windowMenu, helpMenu;
         fileMenu = new JMenu("File");
         treeMenu = new JMenu("Tree");
@@ -94,59 +89,84 @@ class App extends JFrame{
         treeMenu.add(expandBranch);
         treeMenu.add(collapseBranch);
 
-        menuBar.add(fileMenu);
-        menuBar.add(treeMenu);
-        menuBar.add(windowMenu);
-        menuBar.add(helpMenu);
-        this.setJMenuBar(menuBar);
+        mb.add(fileMenu);
+        mb.add(treeMenu);
+        mb.add(windowMenu);
+        mb.add(helpMenu);
+        this.setJMenuBar(mb);
     }
-
-    public void buildToolbar(){
+    private void buildToolbar() {
         File[] paths;
         paths = File.listRoots();
         String[] s1 = new String[paths.length];
         int i = 0;
-        for(File path: paths){
+        for (File path : paths) {
             s1[i] = path.toString();
             i++;
         }
-
-        JComboBox toolbarBox = new JComboBox(s1);
+        toolbarBox = new JComboBox(s1);
         topPanel.add(toolbarBox);
         topPanel.add(details);
         topPanel.add(simple);
+        toolbarBox.addActionListener(new toolbarBoxAction());
         details.addActionListener(new DetailsAction());
         simple.addActionListener(new SimpleAction());
     }
 
-    public void buildStatusBar(String s){
+    private void buildStatusBar(String currentDrive){
         statusBar.removeAll();
-        File file = new File(s);
-        System.out.println(file.getName());
-
-        double usedSpace =  file.getUsableSpace();
-        double freeSpace =  file.getFreeSpace();
-        double totalSpace =  file.getTotalSpace();
-
-        System.out.println(usedSpace);
-        System.out.println(freeSpace);
-        System.out.println(totalSpace);
-
-
-        JLabel status = new JLabel("Current Drive: " + s + " Free Space: " + freeSpace + " Used Space: " + usedSpace + " Total Space: " + totalSpace);
+        File file = new File(currentDrive);
+        int usedSpace = (int) (file.getUsableSpace()/(1024 * 1024 * 1024));
+        int totalSpace = (int) (file.getTotalSpace()/(1024 * 1024 * 1024));
+        int freeSpace = totalSpace - usedSpace;
+        JLabel status = new JLabel("Current Drive: " + currentDrive + " Free Space: " + freeSpace + "GB" + " Used Space: " + usedSpace + "GB" + " Total Space: " + totalSpace + "GB");
         statusBar.add(status);
-        panel.add(statusBar, BorderLayout.SOUTH);
+        mainPanel.add(statusBar, BorderLayout.SOUTH);
     }
-   
-    private class toolbarBoxAction implements ActionListener{
+
+    public App() {
+        //IDk if we're allowed to assign a string to the statusBar JLabel in the constructor
+        mainPanel = new JPanel();
+        topPanel = new JPanel();
+        mb = new JMenuBar();
+        toolbarPanel = new JPanel();
+        desktopPane = new JDesktopPane();
+        statusBar = new JMenuBar();
+        details = new JButton("Details");
+        simple = new JButton("Simple");
+    }
+
+    public void go() {
+
+        mainPanel.setLayout(new BorderLayout());
+        this.setTitle("277 File Manager");
+        buildMenuBar();
+        buildToolbar();
+        buildStatusBar("C:");
+
+
+
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+        mainPanel.add(desktopPane, BorderLayout.CENTER);
+        //FILEFRAME TAKES IN ARGUMENTS NOW
+        FileFrame ff = new FileFrame("C:\\",0,0);
+        desktopPane.add(ff);
+        this.add(mainPanel);
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public class toolbarBoxAction implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e){
             String s = (String) toolbarBox.getSelectedItem();
             System.out.println("you selected " + s);
             buildStatusBar(s);
+            mainPanel.revalidate();
         }
     }
-    
+
     private class SimpleAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e){
@@ -157,19 +177,20 @@ class App extends JFrame{
     private class DetailsAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e){
-            System.out.println("You pressed Details");
+            //FilePanel
         }
     }
 
     private class RenameAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e){
-            DataBack dbdlg = new DataBack();
+
+           // DataBack dbdlg = new DataBack();
             //(DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-            dbdlg.setFromTextField("This is supposed to be the file name u want to rename");//Chnage to file name
-            dbdlg.setVisible(true);
-            String toField = dbdlg.getToTextField();
-            System.out.println("New File name:" + toField);
+            //dbdlg.setFromTextField("This is supposed to be the file name u want to rename");//Chnage to file name
+           // dbdlg.setVisible(true);
+           // String toField = dbdlg.getToTextField();
+
         }
     }
 
@@ -204,29 +225,29 @@ class App extends JFrame{
     private class ExpandBranchAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e){
-            System.out.println("You pressed ExpandBranch");
+            //ff.getLeftPanel().expandTree();
         }
     }
 
     private class CollpaseBranchAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e){
-            System.out.println("You pressed Collapse Branch");
+            // ff.getLeftPanel().collapseTree();
         }
     }
 
-    private class NewAction implements ActionListener {
+    private class NewAction implements ActionListener {//NEEDS TO BE FIXED
         @Override
         public void actionPerformed(ActionEvent e){
-            FileFrame Frame = new FileFrame();
-            desktopPane.add(Frame);// Needs to work for the right directory
+            // FileFrame ff = new FileFrame();
+            // desktopPane.add(ff);
         }
     }
 
     private class CascadeAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e){
-           JInternalFrame[] cascade = desktopPane.getAllFrames();
+            JInternalFrame[] cascade = desktopPane.getAllFrames();
             int i = 10;
             int j = 10;
             for (JInternalFrame k : cascade){
@@ -241,7 +262,8 @@ class App extends JFrame{
     private class HelpAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e){
-            System.out.println("You pressed Collapse Branch");
+            creativeHelpDLG dlg = new creativeHelpDLG();
+            dlg.setVisible(true);
         }
     }
 
@@ -252,5 +274,4 @@ class App extends JFrame{
             dlg.setVisible(true);
         }
     }
-
 }
