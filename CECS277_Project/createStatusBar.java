59d1abdee2a6da09c00e428de46e1a4ecc75ad82
iyadhosn <80491883+iyadhosn@users.noreply.github.com>
2021-05-01 13:24:50 -0700

    Delete createStatusBar.java

--- CECS277_Project/createStatusBar.java
+++
@@ -1,8 +0,0 @@
-package CECS277_Project;
-import java.io.*;
-public class createStatusBar{
-    public void setDircetory(String directory){
-        File file = new File(directory);
-        //NEED TO FINISH THIS
-    }
-}
